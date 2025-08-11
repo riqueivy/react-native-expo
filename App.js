a7e38527350f855bd3cbdf8e8d38e50f2a0db51e
riqueivy <riqueivymeneses@gmail.com>
2025-08-11 00:02:33 -0300

    novas atualizaçoes 10/08

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Button, Image, Text, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  clicar = () => {
+    alert("Botão clicado!");
+  }
+
+  render() {
+    let nome = "mateus";
+    let img = "https://images.unsplash.com/photo-1607746882042-944635dfe10e?auto=format&fit=crop&w=800&q=60";
+
+    return (
+      <View>
+        <Text style={{ color: 'red', fontSize: 12, margin: 12 }}>ola mundo</Text>
+        <Text>{nome}</Text>
+
+        <Text style={estilo.texto}>hallo word</Text>
+        <Text style={estilo.area}>Área com margem</Text>
+
+        <Image source={{ uri: img }} style={{ width: 100, height: 100 }} />
+
+        <Button title="clicar" onPress={this.clicar} />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const estilo = StyleSheet.create({
+  area: {
+    marginTop: 40
   },
+  texto: {
+    fontSize: 25,
+    color: '#FF0000'
+  },
+  fundo: {
+    textAlign: 'center',
+    backgroundColor: '#99CC32'
+  }
 });
+
+export default App;
